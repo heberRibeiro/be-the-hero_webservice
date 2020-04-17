acc5259f68a48ec4c7be427289ef45238b04ceff
Heber Ribeiro <heber.r@outlook.com>
2020-04-17 11:27:13 -0300

    Project started

---
+++ src/main/java/br/com/heberribeiro/beTheHeroWebService/BeTheHeroWebServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.heberribeiro.beTheHeroWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTheHeroWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTheHeroWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/heberribeiro/beTheHeroWebService/BeTheHeroWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heberribeiro.beTheHeroWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTheHeroWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
