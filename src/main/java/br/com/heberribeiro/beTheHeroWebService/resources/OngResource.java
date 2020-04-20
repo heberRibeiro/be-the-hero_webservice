a0954e24a7b24cdcbcdf67256da3c1c7dc1e32c3
Heber Ribeiro <heber.r@outlook.com>
2020-04-20 14:21:55 -0300

    Resources layer - OngResource - insert

--- src/main/java/br/com/heberribeiro/beTheHeroWebService/resources/OngResource.java
+++ src/main/java/br/com/heberribeiro/beTheHeroWebService/resources/OngResource.java
@@ -1,12 +1,16 @@
 package br.com.heberribeiro.beTheHeroWebService.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.heberribeiro.beTheHeroWebService.domain.Ong;
 import br.com.heberribeiro.beTheHeroWebService.repositories.OngRepository;
@@ -24,6 +28,16 @@ public class OngResource {
 		List<Ong> ongs = ongRepository.findAll();
 		
 		return ResponseEntity.ok().body(ongs);		
+	}
+	
+	@PostMapping(value = "/ongs")
+	public ResponseEntity<Void> insert(@RequestBody Ong ong) {
+		ong.setId(null);
+		Ong obj = ongRepository.save(ong);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
