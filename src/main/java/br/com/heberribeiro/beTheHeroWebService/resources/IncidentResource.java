53044bb784f51e07aeecce425b93868081c24cda
Heber Ribeiro <heber.r@outlook.com>
2020-04-21 10:12:54 -0300

    Resources layer - IncidentResource - insert

--- src/main/java/br/com/heberribeiro/beTheHeroWebService/resources/IncidentResource.java
+++ src/main/java/br/com/heberribeiro/beTheHeroWebService/resources/IncidentResource.java
@@ -1,15 +1,22 @@
 package br.com.heberribeiro.beTheHeroWebService.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.heberribeiro.beTheHeroWebService.domain.Incident;
+import br.com.heberribeiro.beTheHeroWebService.domain.Ong;
 import br.com.heberribeiro.beTheHeroWebService.repositories.IncidentRepository;
+import br.com.heberribeiro.beTheHeroWebService.repositories.OngRepository;
 
 @RestController
 @RequestMapping
@@ -18,12 +25,29 @@ public class IncidentResource {
 	@Autowired
 	private IncidentRepository incidentRepository;
 	
+	@Autowired
+	private OngRepository ongRepository;
+	
 	@GetMapping(value = "/incidents")
 	public ResponseEntity<List<Incident>> findAll() {
 		
 		List<Incident> incidents = incidentRepository.findAll();
 		
 		return ResponseEntity.ok().body(incidents);		
+	}
+	
+	@PostMapping(value = "/incidents")
+	public ResponseEntity<Void> insert(@RequestBody Incident incident, @RequestHeader Integer ong_id) {
+		incident.setId(null);
+		Ong ong = ongRepository.getOne(ong_id);
+		incident.setOng(ong);
+		ong.getIncidents().add(incident);
+		
+		Incident obj = incidentRepository.save(incident);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();		
 	}
 
 }
