40420bcb43a84808c980aaf65f42d17df13000c0
Heber Ribeiro <heber.r@outlook.com>
2020-04-22 20:29:15 -0300

    Exception object not found

--- src/main/java/br/com/heberribeiro/beTheHeroWebService/services/IncidentService.java
+++ src/main/java/br/com/heberribeiro/beTheHeroWebService/services/IncidentService.java
@@ -3,38 +3,44 @@ package br.com.heberribeiro.beTheHeroWebService.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.heberribeiro.beTheHeroWebService.domain.Incident;
 import br.com.heberribeiro.beTheHeroWebService.domain.Ong;
 import br.com.heberribeiro.beTheHeroWebService.repositories.IncidentRepository;
 import br.com.heberribeiro.beTheHeroWebService.repositories.OngRepository;
+import br.com.heberribeiro.beTheHeroWebService.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class IncidentService {
-	
+
 	@Autowired
 	private IncidentRepository incidentRepository;
-	
+
 	@Autowired
 	private OngRepository ongRepository;
-	
+
 	public List<Incident> findAll() {
 		return incidentRepository.findAll();
 	}
-	
+
 	public Incident insert(Incident incident, Integer ong_id) {
 		incident.setId(null);
 		Ong ong = ongRepository.getOne(ong_id);
 		incident.setOng(ong);
 		ong.getIncidents().add(incident);
 		Incident obj = incidentRepository.save(incident);
-		
+
 		return obj;
 	}
-	
+
 	public void delete(Integer id) {
-		incidentRepository.deleteById(id);
+		try {
+			incidentRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Object not exist in data base");
+		}
 	}
 
 }
